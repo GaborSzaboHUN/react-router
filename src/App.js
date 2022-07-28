3c4ad7ada3032d70b3ed018c61e1262a549bae25
GaborSzaboHUN <66555347+GaborSzaboHUN@users.noreply.github.com>
2022-07-28 10:38:47 +0200

    First steps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+
+            <BrowserRouter>
+
+                <header>
+                    <nav>
+                        <Link to="/">Home</Link>
+                        <Link to="/about">About</Link>
+                        <Link to="/contact">Contact</Link>
+                    </nav>
+                </header>
+
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+
+            </BrowserRouter>
+
+        </div>
+
+    );
 }
 
 export default App;
---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+//rfc - React Functional Component
+import React from 'react'
+
+function About() {
+    return (
+        <div>About</div>
+    )
+}
+
+export default About
